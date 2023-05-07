55175748b0aeaffd815a8f15370dfe58f0ef73d7
Adbelrhman Gomaa <abdelrhman.gomaa@baianat.com>
2023-05-07 14:33:43 +0300

    feat: database provider

---
+++ src/_common/database/database.model.repositories.ts
@@ -0,0 +1,7 @@
+export enum Repositories {
+    UsersRepository = 'UsersRepository',
+    UserSocialAccountsRepository = 'UserSocialAccountsRepository',
+    UserVerificationCodesRepository = 'UserVerificationCodesRepository',
+    ProductsRepository = 'ProductsRepository',
+    InvoicesRepository = 'InvoicesRepository',
+}
