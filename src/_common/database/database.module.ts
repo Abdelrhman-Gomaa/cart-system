614f847cef48e9a1565cecd6fc7e2aeec5a0ecfb
Abdelrhman Gomaa <abdogomaa1102@gmail.com>
2023-05-08 22:08:41 +0200

    feat: cart model in database

--- src/_common/database/database.module.ts
+++ src/_common/database/database.module.ts
@@ -1,7 +1,8 @@
 import { Module } from '@nestjs/common';
 import { databaseProviders } from './database.providers';
+import { ModelsProvider } from './models.providers';
 @Module({
-    providers: [...databaseProviders],
-    exports: [...databaseProviders]
+    providers: [...databaseProviders, ...ModelsProvider],
+    exports: [...databaseProviders, ...ModelsProvider]
 })
 export class DatabaseModule {}
--- src/cart/model/cart.model.ts
+++ src/cart/model/cart.model.ts
@@ -33,7 +33,7 @@ export class Cart extends Model {
 
     @AllowNull(true)
     @Column({ type: DataType.JSONB })
-    @Field(() => ContextInfoType, {nullable: true})
+    @Field(() => PriceType, {nullable: true})
     price?: PriceType;
 
     @CreatedAt
