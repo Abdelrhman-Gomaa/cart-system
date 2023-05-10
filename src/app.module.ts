f5ec8c087c936d0a0521a6163c31b9472851712f
Adbelrhman Gomaa <abdelrhman.gomaa@baianat.com>
2023-05-10 11:57:30 +0300

    fix: graphql configuration

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,7 +2,7 @@ import { Module } from '@nestjs/common';
 import { DatabaseModule } from './_common/database/database.module';
 import { UserModule } from './user/user.module';
 import { ConfigModule } from '@nestjs/config';
-import { GraphQLModule } from '@nestjs/graphql';
+import { Context, GraphQLModule } from '@nestjs/graphql';
 import { ApolloDriverConfig, ApolloDriver } from '@nestjs/apollo';
 import { join } from 'path';
 import { MailModule } from './_common/mail/mail.module';
@@ -10,16 +10,22 @@ import { NestBullModule } from './_common/bull/bull.module';
 import { ProductModule } from './product/product.module';
 import { InvoiceModule } from './invoice/invoice.module';
 import { CartModule } from './cart/cart.module';
+import { Request } from 'express';
+import { TokenPayload } from './auth/auth-token-payload.interface';
+import { ContextModule } from './_common/context/context.module';
+import { GqlConfigService } from './_common/graphql/graphql.provider';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       envFilePath: '.env',
     }),
-    GraphQLModule.forRoot<ApolloDriverConfig>({
+    GraphQLModule.forRootAsync<ApolloDriverConfig>({
       driver: ApolloDriver,
-      autoSchemaFile: join(process.cwd(), 'schema.gql'),
-      sortSchema: true,
+      useClass: GqlConfigService,
+      // autoSchemaFile: join(process.cwd(), 'schema.gql'),
+      // sortSchema: true,
+      imports: [ContextModule]
     }),
     DatabaseModule,
     UserModule,
